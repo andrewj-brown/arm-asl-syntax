1b93a33e8b830d140d60a8aa415a5fb5c0be7c84
andrewj-brown <92134285+andrewj-brown@users.noreply.github.com>
2022-11-29 14:15:34 +1000

    more client setup stuff

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -3,13 +3,31 @@
 import path = require('path');
 import vscode = require('vscode');
 import {
+    CancellationToken,
+    DefinitionRequest,
+    Disposable,
+    DocumentSelector,
 	LanguageClient,
 	LanguageClientOptions,
 	ServerOptions,
 	TransportKind
 } from 'vscode-languageclient/node';
 
+let selector: DocumentSelector = [{
+    language: 'arm-asl'
+}]
+
+let provider: vscode.DefinitionProvider = {
+    provideDefinition(document: vscode.TextDocument, position: vscode.Position, token: CancellationToken): vscode.ProviderResult<vscode.Definition> {
+        let file: vscode.Uri = vscode.Uri.file("foo");
+        let pos: vscode.Position = new vscode.Position(0, 0);
+
+        return (new vscode.Location(file, pos) as vscode.Definition)
+    }
+}
+
 let client: LanguageClient;
+let defDisposer: Disposable;
 
 export function activate(context: vscode.ExtensionContext) {
     console.log("Activating arm-asl client");
@@ -45,11 +63,17 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Start the client. This will also launch the server
 	client.start();
+
+    defDisposer = vscode.languages.registerDefinitionProvider(selector, provider);
 }
 
 export function deactivate(): Thenable<void> | undefined {
 	if (!client) {
 		return undefined;
 	}
-	return client.stop();
+    if (defDisposer) {
+        defDisposer.dispose();
+    }
+       
+    return client.stop();
 }
